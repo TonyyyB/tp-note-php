b331119c1c4d69b479bf3f55093a0520843f7e40
MatheoLobjois <matheo.lobjois@etu.univ-orleans.fr>
2025-01-10 10:33:04 +0100

    creation du PDO

--- _inc/classes/Model/DataSources/DataBaseProvider.php
+++ _inc/classes/Model/DataSources/DataBaseProvider.php
@@ -1,2 +1,13 @@
 <?php
+namespace Model\DataSources;
 declare(strict_types=1);
+
+$pdo = null;
+try {
+    $pdo = $pdo = new PDO('sqlite:db.sqlite');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur: " . $e->getMessage() . "<br/>";
+    die();
+}
+
