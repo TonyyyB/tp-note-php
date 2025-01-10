4baadf6a6a140841d6e40ab0c90864914452331e
MatheoLobjois <matheo.lobjois@etu.univ-orleans.fr>
2025-01-10 10:48:59 +0100

    création de la classe DataBaseProvider

--- _inc/classes/Model/DataSources/DataBaseProvider.php
+++ _inc/classes/Model/DataSources/DataBaseProvider.php
@@ -1,13 +1,30 @@
 <?php
 namespace Model\DataSources;
 declare(strict_types=1);
+class DataBaseProvider{
 
-$pdo = null;
-try {
-    $pdo = $pdo = new PDO('sqlite:db.sqlite');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erreur: " . $e->getMessage() . "<br/>";
-    die();
+    private PDO $pdo;
+
+    public function __construct(){
+        $this->pdo = null;
+        try {
+            $this->pdo = new PDO('sqlite:db.sqlite');
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Erreur: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+    
+    public function ajouterJoueur(string $nom,string $prenom):void{
+        $sql = $this->pdo->prepare("INSERT INTO JOUEUR(nom,prenom) VALUES (?, ?);");
+        $sql-> execute($nom,$prenom);
+    }
+    
+    public function ajouterScore(string $idJ,int $score): void{
+        $req = $this->pdo->prepare("INSERT INTO A(idJ,score) VALUES (?,?);");
+        $req-> execute($idJ,$score);
+    }
+    
+    
 }
-
